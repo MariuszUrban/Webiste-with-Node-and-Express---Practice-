fade06c2b3cde7ad1ec52e76be123770531fe90b
Mariusz <urban.mariusz@icloud.com>
2021-01-12 10:41:56 +0100

    done

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express(); 
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("siemaneczko :) ")
+} )
+
+app.listen(port, () => {
+    console.log(`express server is listening on port ${port}`); 
+})
