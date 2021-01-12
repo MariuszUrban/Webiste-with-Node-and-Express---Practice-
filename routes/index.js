f1da7f28d2db0998ace0abb4ecaf302a6cdde29b
Mariusz <urban.mariusz@icloud.com>
2021-01-12 17:06:56 +0100

    done

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+
+const speakersRoute = require("./speakers"); 
+const feedbackRoute = require("./feedback"); 
+
+
+const router = express.Router(); 
+
+module.exports  = params => {
+    router.get("/", (req, res) => {
+        if(!req.session.visitcount){
+            req.session.visitcount = 0; 
+        }
+        req.session.visitcount += 1; 
+        console.log("🚀 ~ router.get ~ req.session.visitcount", req.session.visitcount);
+        
+        res.render('pages/index', {pageTitle: "Welcome"})
+    } ); 
+
+    router.use('/speakers', speakersRoute(params));
+    router.use('/feedback', feedbackRoute(params));
+
+    return router;
+}
+
+
